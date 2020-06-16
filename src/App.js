bd3f0480864b3731523baf6ef433ff38e74bf24c
sammiefrog <sammiefrog@live.com>
2020-06-16 16:31:03 -0500

    added a search field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Table from "./components/Table";
 import Menu from "./components/Menu";
+import TextField from "./components/TextField";
 // import Container from "./components/Container";
 import Container from "@material-ui/core/Container";
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Menu />
       <Container maxWidth="md">
+        <TextField />
         <Table />
       </Container>
     </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -15,16 +15,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(thumbnail, firstName, lastName, email) {
+  return { thumbnail, firstName, lastName, email };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("picture eventually", "Bob", "Barker", "bbarker@gmail.com"),
+  createData("picture eventually", "Bob", "Barker", "bbarker@gmail.com"),
+  createData("picture eventually", "Bob", "Barker", "bbarker@gmail.com"),
+  createData("picture eventually", "Bob", "Barker", "bbarker@gmail.com"),
+  createData("picture eventually", "Bob", "Barker", "bbarker@gmail.com"),
 ];
 
 export default function SimpleTable() {
@@ -35,23 +35,23 @@ export default function SimpleTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Employees</TableCell>
+            {/* <TableCell align="right">Thumbnail</TableCell> */}
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">E-Mail</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.thumbnail}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              {/* <TableCell align="right">{row.thumbnail}</TableCell> */}
+              <TableCell align="right">{row.firstName}</TableCell>
+              <TableCell align="right">{row.lastName}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
             </TableRow>
           ))}
         </TableBody>
